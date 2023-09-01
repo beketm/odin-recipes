75609cd68e77f4a9d84ba9242e3a274831b66bb7
Beket Myrzanov <beket0101@gmail.com>
2023-09-01 18:37:45 +0600

    homepage changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
   createHashRouter,
-  // createBrowserRouter,
+  createBrowserRouter,
   RouterProvider,
   // Route,
   Link,
